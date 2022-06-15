039d9ab89f801fe575c1175496730bd931cc44b8
unknown <formacio@PC-1062619.formacio.local>
2022-06-15 09:57:18 +0200

    callback hell

--- callback_hell.js
+++ callback_hell.js
@@ -16,7 +16,7 @@ const reverseText = str =>
   .join("");
 
 // Read and reverse contents of text files in a directory
-readdir(inbox, (error, files) => {
+/* readdir(inbox, (error, files) => {
   if (error) return console.log("Error: Folder inaccessible");
   files.forEach(file => {
     readFile(join(inbox, file), "utf8", (error, data) => {
@@ -27,4 +27,47 @@ readdir(inbox, (error, files) => {
       });
     });
   });
-});
+}); */
+
+
+
+const promiseReadDir = (inbox) =>{
+return new Promise((resolve, reject) => {
+    readdir(inbox, (error, files) => {
+      error
+      ? reject(new Error ("Error: Folder inaccessible"))
+      : resolve(files)
+    });
+  })
+}
+
+
+const promiseReadFile = (inbox, file) => {
+  return new Promise((resolve, reject) => {
+    readFile(join(inbox, file), "utf8", (error, data) => {
+      error
+      ? reject(new Error ("Error: File error"))
+      : resolve(data)
+    });
+  }) 
+}
+
+const promiseWriteFile = (outbox, file, data) => {
+  return new Promise((resolve, reject) => {
+    writeFile(join(outbox, file), reverseText(data), error => {
+      error
+      ? reject(new Error ("Error: File could not be saved!"))
+      : resolve(console.log(`${file} was successfully saved in the outbox!`))
+      });
+    })
+  }
+  
+promiseReadDir(inbox)
+.then((files) => {
+  files.forEach(file => {
+    promiseReadFile(inbox, file)
+    .then((data) => {
+      promiseWriteFile(outbox, file, data)
+    })
+  })
+})
