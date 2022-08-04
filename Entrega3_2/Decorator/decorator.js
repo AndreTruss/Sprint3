894b0cadd5d3614cfe19ddafc8f799c88295ec5f
AndreTruss <andrea.trussardi@gmail.com>
2022-08-04 14:43:24 +0200

    fixed decorator

--- Entrega3_2/Decorator/decorator.js
+++ Entrega3_2/Decorator/decorator.js
@@ -3,10 +3,11 @@ const conversions = require( './currency_conversions.json' );
 function converseInEur( product ) {
     const keys = Object.keys( conversions )
     let key = keys.find( el => el.slice(0, 3) == product.currency )
-    //console.log( conversions[key] )
+    //console.log( conversions.key, key, conversions.CAD_EUR, conversions[key] )
         
     let priceInEur = product.price * conversions[key]
-    console.log ( `${ product.name } has a price of ${ Math.round( priceInEur * 100 ) / 100 } EUR`) 
+    product.priceInEur = Math.round( priceInEur * 100 ) / 100
+    console.log( product )
 }
 
 module.exports = { converseInEur }
